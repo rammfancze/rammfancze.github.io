c2ef3d7c71933af4a8a27c3f41d9c939ee150cfd
rammfancze <102330884+rammfancze@users.noreply.github.com>
2026-06-28 04:15:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -126,6 +126,7 @@ function vykresli(data, targetId, jeToRecorded) {
             const formatText = col[5] || '';
             const audioUrl = col[6] ? col[6].trim() : '';
             const imgUrl = col[7] ? col[7].trim() : '';
+            const datumObj = parsujDatum(col[1]);
             const denNazev = datumObj ? getDenVTydnu(datumObj) : '';
 
             tr.innerHTML = `
